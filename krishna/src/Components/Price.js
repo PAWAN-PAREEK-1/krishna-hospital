d03d448f907c4e63e99e4e572fba07f46591a55b
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-09-15 16:50:14 +0530

    price section

--- krishna/src/Components/Price.js
+++ krishna/src/Components/Price.js
@@ -1,36 +1,184 @@
-import React from 'react'
+import React from "react";
 import "../Style/Price.css";
+import leftarrow from "../Assets/img/leftarrow.svg";
 
 const Price = () => {
   return (
     <div>
-        <div className="index">
-            <div className="main-price">
-                <div className="price-up">
-                <div className="med-top med-top2">
+      <div className="index">
+        <div className="main-price">
+          <div className="price-up">
+            <div className="med-top med-top2">
               <div className="span-box about-span ">
                 <div className="span-box-1 about-span-box med-span-box"></div>
                 <h3>Schedule a Home Visit</h3>
               </div>
               <h1>Free Home Visit</h1>
             </div>
-                </div>
+          </div>
 
-                <div className="price-bottom">
-                    <div className="price-box">
-                        <h2>Basic Health Checkup</h2>
-                        <div className="right-price">
-                            <h3>Rate @ 1800/-</h3>
-                            <h4>Just @ 1200/-</h4>
-                        </div>
+          <div className="price-bottom">
+            <div className="price-box">
+              <div className="price-lable">
+                <h1>
+                  70 <br />
+                  TEST
+                </h1>
+              </div>
+              <h2>Basic Health Checkup</h2>
+              <div className="right-price">
+                <h5>Rate @ 1800/-</h5>
+                <h4>Just @ 1200/-</h4>
+              </div>
+              <hr />
 
-                    </div>
+              <h3>
+                <img src={leftarrow} alt="" />
+                Complete Haemogram (28 Test)
+              </h3>
+              <br />
+              <h3>
+                <img src={leftarrow} alt="" />
+                Lipid Profile (7 Test)
+              </h3>
+              <br />
+              <h3>
+                <img src={leftarrow} alt="" />
+                Liver Function Test (1 Test)
+              </h3>
+              <br />
+              <h3>
+                <img src={leftarrow} alt="" />
+                Kidney Function Test (1 Test)
+              </h3>
+              <br />
+              <h3>
+                <img src={leftarrow} alt="" />
+                Diabetic Test
+              </h3>
+              <br />
+              <h3>
+                <img src={leftarrow} alt="" />
+                Arthritis Test
+              </h3>
+              <br />
+              <h3>
+                <img src={leftarrow} alt="" />
+                Urine Analysis
+              </h3>
+              <br />
 
-                </div>
+              <h6>Free Dietician Consulting</h6>
             </div>
-        </div>
-    </div>
-  )
-}
+            <div className="price-box">
+              <div className="price-lable">
+                <h1>
+                  80 <br />
+                  TEST
+                </h1>
+              </div>
+              <h2>General Health Checkup</h2>
+              <div className="right-price">
+                <h5>Rate @ 4000/-</h5>
+                <h4>Just @ 3000/-</h4>
+              </div>
+              <hr />
 
-export default Price
+              <h3>
+                <img src={leftarrow} alt="" />
+                Complete Haemogram (28 Test)
+              </h3>
+              <br />
+              <h3>
+                <img src={leftarrow} alt="" />
+                Lipid Profile (7 Test)
+              </h3>
+              <br />
+              <h3>
+                <img src={leftarrow} alt="" />
+                Liver Function Test (1 Test)
+              </h3>
+              <br />
+              <h3>
+                <img src={leftarrow} alt="" />
+                Kidney Function Test (1 Test)
+              </h3>
+              <br />
+              <h3>
+                <img src={leftarrow} alt="" />
+                Diabetic Test
+              </h3>
+              <br />
+              <h3>
+                <img src={leftarrow} alt="" />
+                Arthritis Test
+              </h3>
+              <br />
+              <h3>
+                <img src={leftarrow} alt="" />
+                Urine Analysis
+              </h3>
+              <br />
+
+              <h6>Free Dietician Consulting</h6>
+            </div>
+            <div className="price-box">
+              <div className="price-lable">
+                <h1>
+                  93 <br />
+                  TEST
+                </h1>
+              </div>
+              <h2>Advance Health Checkup</h2>
+              <div className="right-price">
+                <h5>Rate @ 1800/-</h5>
+                <h4>Just @ 1200/-</h4>
+              </div>
+              <hr />
+
+              <h3>
+                <img src={leftarrow} alt="" />
+                Complete Haemogram (28 Test)
+              </h3>
+              <br />
+              <h3>
+                <img src={leftarrow} alt="" />
+                Lipid Profile (7 Test)
+              </h3>
+              <br />
+              <h3>
+                <img src={leftarrow} alt="" />
+                Liver Function Test (1 Test)
+              </h3>
+              <br />
+              <h3>
+                <img src={leftarrow} alt="" />
+                Kidney Function Test (1 Test)
+              </h3>
+              <br />
+              <h3>
+                <img src={leftarrow} alt="" />
+                Diabetic Test
+              </h3>
+              <br />
+              <h3>
+                <img src={leftarrow} alt="" />
+                Arthritis Test
+              </h3>
+              <br />
+              <h3>
+                <img src={leftarrow} alt="" />
+                Urine Analysis
+              </h3>
+              <br />
+
+              <h6>Free Dietician Consulting</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Price;
