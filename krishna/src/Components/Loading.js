9f5e90d94273bf54fca54147155b41f582fcbbd5
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-09-18 11:27:00 +0530

    loading components

--- krishna/src/Components/Loading.js
+++ krishna/src/Components/Loading.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import "../Style/Loading.css";
 
 const Loading = () => {
   return (
     <>
-    <div>Loading...</div>
+    <div>
+          <div className="loading-com">
+            <h4>
+            <div class="spinner-square">
+        <div class="square-1 square"></div>
+        <div class="square-2 square"></div>
+        <div class="square-3 square"></div>
+</div>
+            </h4>
+          </div>
+
+      </div>
     </>
   )
 }
